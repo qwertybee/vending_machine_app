67f7269eaa46b65f46905e1f2c9c0fd645c70a99
Rinradee Sae-lao <rinradee.sae@gmail.com>
2023-01-27 22:57:47 +0700

    fixed null operator for required case

--- vending_machine_django/accounts/models.py
+++ vending_machine_django/accounts/models.py
@@ -2,18 +2,18 @@ from django.db import models
 
 
 class Vending_Machine(models.Model):
-    name = models.CharField(max_length=20, null=True)
-    location = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    name = models.CharField(max_length=20)
+    location = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=20, null=True)
-    price = models.PositiveIntegerField(null=True)
-    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    name = models.CharField(max_length=20)
+    price = models.PositiveIntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
@@ -22,7 +22,7 @@ class Product(models.Model):
 class Order(models.Model):
     vending_machine = models.ForeignKey(Vending_Machine, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
-    quantity = models.PositiveIntegerField(null=True)
+    quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return self.product.name
